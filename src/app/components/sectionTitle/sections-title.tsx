6fb0f4b38aecc7364d02f0c1565f1a2874b19d12
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-09-03 19:35:51 -0300

    feat: section title component

---
+++ src/app/components/sectionTitle/sections-title.tsx
@@ -0,0 +1,9 @@
+interface SectionTitleProps {
+    text: string
+}
+
+export function SectionTitle({ text }: SectionTitleProps) {
+    return (
+        <h3 className="Section-title">{text}</h3>
+    )
+}
