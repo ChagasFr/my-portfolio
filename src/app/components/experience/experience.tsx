708a5c8add7167bed6274242578eddf05e403411
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-09-02 22:13:29 -0300

    feat: experience component

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,19 +1,9 @@
-import Image from "next/image"
-import { SectionTitle } from "../sectionTitle/section-title"
-
-import "./experience.scss"
-import { Skill } from "../skill/skill"
-
 export function Experience() {
     return (
         <div className="experience">
-            <SectionTitle text="Experience" />
-            <p>3 years working as a Software Developer, in companys such as Itaú, Stone and Aftersale.</p>
+            <h3>Experience</h3>
+            <p>1 year of experience as a software developer, internship at Banco Safra, and development experience gained during college and personal projects.</p>
             <div className="experience-time">
-                <Skill image="/react.png" measure={2} years="2 years" />
-                <Skill image="/ts.png" measure={3} years="3 years" />
-                <Skill image="/js.png" measure={3} years="3 years" />
-                <Skill image="/java.png" measure={1} years="1 year" />
             </div>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,19 @@ export default function Home() {
   return (
     <main className="container">
       <Header />
-      <div className="experience">
-        <h3>Experience</h3>
-        <p>1 year of experience as a software developer, internship at Banco Safra, and development experience gained during college and personal projects.</p>
-        <div className="experience-time">
-
+      <div className="infos">
+        <div className="lenguages-info">
+          <span>Pt-Br - Native Speaker</span>
         </div>
-        <div className="infos">
-          <div className="lenguages-info">
-            <span>Pt-Br - Native Speaker</span>
-          </div>
-          <h3>Education</h3>
-          <div className="educational-info">
-            <span>Engenharia da Computação bachelors degree - Universidade São Judas Tadeu</span>
-          </div>
+        <h3>Education</h3>
+        <div className="educational-info">
+          <span>Engenharia da Computação bachelors degree - Universidade São Judas Tadeu</span>
         </div>
-        <div className="buttons">
-          <div className="social"></div>
-        </div>
-        <button>contact Me!</button>
       </div>
+      <div className="buttons">
+        <div className="social"></div>
+      </div>
+      <button>contact Me!</button>
     </main>
   );
 }
