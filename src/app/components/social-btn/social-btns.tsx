9da003dfe26c4a649b660b24e6a5b5fb590342c2
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-09-04 20:39:08 -0300

    feat: icons buttons

---
+++ src/app/components/social-btn/social-btns.tsx
@@ -0,0 +1,14 @@
+interface SocialBtnsProps {
+
+}
+export default function SocialBtns(props: SocialBtnsProps) {
+    return (
+        <div className="social">
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import { Experience } from "./components/experience/experience";
 import Header from "./components/header/header";
+import { EmailIcon } from "./components/icons/email-icon";
 import { Info } from "./components/information/information";
-
-import Image from "next/image";
 
 import "./styles/home.scss"
 
@@ -17,13 +16,7 @@ export default function Home() {
       </div>
       <button className="bnt-primary">
         contact Me!
-        <Image
-          src="/public/me.jpg"
-          alt="Vercel Logo"
-          width={325}
-          height={277}
-          priority
-        />
+        <EmailIcon />
       </button>
     </main>
   );
